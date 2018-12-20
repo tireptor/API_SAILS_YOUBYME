472ed76c7bc96f1088ab1825d31a50e33de15f24
Unknown <imeraz_@hotmail.fr>
2018-12-20 09:34:10 +0100

    API User

---
+++ youbyme_api/api/models/User.js
@@ -0,0 +1,37 @@
+/**
+ * User.js
+ *
+ * @description :: A model definition represents a database table/collection.
+ * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
+ */
+
+module.exports = {
+  tableName: 't_personne',
+  attributes: {
+    updatedAt: false,
+    createdAt: false, 
+    id: {
+      type:'number', 
+      autoIncrement: true, 
+      columnName:'id_t_personne'
+    },
+    groupe: {
+      type:'string',
+      columnName:'id_t_groupe'
+    },
+    nom: {
+      type:'string',
+      columnName:'nom_t_personne'
+    },
+    prenom: {
+      type:'string',
+      columnName:'prenom_t_personne'
+    },
+    email: {
+      type:'string', 
+      columnName:'email_t_personne',
+      unique: true,
+    },
+  },
+
+};
