9e2a8efbb399678553138a166ed811dd1c2f7e59
Unknown <imeraz_@hotmail.fr>
2018-12-21 09:42:55 +0100

    Commit UserVote

--- youbyme_api/api/models/UserVote.js
+++ youbyme_api/api/models/UserVote.js
@@ -1,5 +1,5 @@
 /**
- * Softskill.js
+ * UserVote.js
  *
  * @description :: A model definition represents a database table/collection.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
